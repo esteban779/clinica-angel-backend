63631704d9196784254b4d5df2821f1290f967c8
Esteban <lesteban.ramos.olort@gmail.com>
2023-09-02 23:22:05 -0500

    Test commit

---
+++ src/main/java/com/capstone/clinica_angel/ClinicaAngelApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.clinica_angel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaAngelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/clinica_angel/ClinicaAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.clinica_angel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaAngelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
