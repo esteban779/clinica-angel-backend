6ba09a4506155ef77f9537b160609a74258238c7
Esteban <lesteban.ramos.olort@gmail.com>
2023-10-14 23:43:49 -0500

    models updated

--- src/main/java/com/capstone/clinica_angel/model/CajeroModel.java
+++ src/main/java/com/capstone/clinica_angel/model/CajeroModel.java
@@ -8,15 +8,13 @@ public class CajeroModel {
     
     private Long id_caje;
     private Long id_trab;
-    private Long id_usua;
     private String estado;
 
     public CajeroModel() { }
 
-    public CajeroModel(Long id_caje, Long id_trab, Long id_usua, String estado) {
+    public CajeroModel(Long id_caje, Long id_trab, String estado) {
         this.id_caje = id_caje;
         this.id_trab = id_trab;
-        this.id_usua = id_usua;
         this.estado = estado;
     }
 }
--- src/main/java/com/capstone/clinica_angel/model/MedicoModel.java
+++ src/main/java/com/capstone/clinica_angel/model/MedicoModel.java
@@ -8,16 +8,14 @@ public class MedicoModel {
     
     private Long id_med;
     private Long id_trab;
-    private Long id_usua;
     private Long id_espec;
     private String estado;
 
     public MedicoModel() { }
 
-    public MedicoModel(Long id_med, Long id_trab, Long id_usua, Long id_espec, String estado) {
+    public MedicoModel(Long id_med, Long id_trab, Long id_espec, String estado) {
         this.id_med = id_med;
         this.id_trab = id_trab;
-        this.id_usua = id_usua;
         this.id_espec = id_espec;
         this.estado = estado;
     }
