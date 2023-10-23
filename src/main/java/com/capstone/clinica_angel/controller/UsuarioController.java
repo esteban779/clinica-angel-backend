9660e853171f7325f6bf6f38fb95b22ba96558d9
EduardoMAT30 <148301030+EduardoMAT30@users.noreply.github.com>
2023-10-22 20:37:03 -0500

    Front Registro

--- src/main/java/com/capstone/clinica_angel/controller/UsuarioController.java
+++ src/main/java/com/capstone/clinica_angel/controller/UsuarioController.java
@@ -2,14 +2,23 @@
 package com.capstone.clinica_angel.controller;
 
 import com.capstone.clinica_angel.service.UsuarioService;
+import org.springframework.ui.Model;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("usuario")
 public class UsuarioController {
     
     private UsuarioService se;
+    
+    @RequestMapping("index")
+    public String inicio() {
+        return "index";
+    }
     
     
 }
