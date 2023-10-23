ba0fa2a2da838740c78fa3b7590b62c50f27537b
EduardoMAT30 <148301030+EduardoMAT30@users.noreply.github.com>
2023-10-23 01:02:10 -0500

    Plantillas
    se agregaron las plantillas de navegacion

--- src/main/java/com/capstone/clinica_angel/controller/UsuarioController.java
+++ src/main/java/com/capstone/clinica_angel/controller/UsuarioController.java
@@ -1,24 +1,36 @@
 
 package com.capstone.clinica_angel.controller;
 
-import com.capstone.clinica_angel.service.UsuarioService;
-import org.springframework.ui.Model;
-
+import com.capstone.clinica_angel.controller.UsuarioController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 @Controller
-@RequestMapping("usuario")
+//@RequestMapping("usuario")
 public class UsuarioController {
     
-    private UsuarioService se;
+//    private UsuarioService se;
     
     @RequestMapping("index")
     public String inicio() {
         return "index";
     }
     
+     @RequestMapping("RegistroCitas")
+    public String registroCitas() {
+        return "RegistroCitas";
+    }
     
+    @RequestMapping("Contacto")
+    public String contacto() {
+        return "Contacto";
+    }
+    
+    @RequestMapping("Emergencia")
+    public String emergencia() {
+        return "Emergencia";
+    }
 }
